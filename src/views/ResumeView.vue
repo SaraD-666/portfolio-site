7714c2ebc3a9ed5883cc5a34b1de7abdb05e5f15
3047322836 <3047322836@qq.com>
2025-12-01 20:46:47 -0500

    updated the resume

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,26 +1,32 @@
 <script>
-    export default {
-        data() {
-            return {}
-		}
-	}
+import myResume from '../files/Jingwen_Dou_Resume.pdf'
+
+export default {
+    data() {
+        return {
+            resumeUrl: myResume
+        }
+    }
+}
 </script>
+
 <template>
     <div class="px-5 py-5 md:px-12 md:py-10 text-left text-amber-50 mx-3">
         <article data-page="about">
             <header>
                 <div
-                        class="text-2xl font-bold text-white mb-10 fadein-bot title-section flex items-center justify-center flex-col">
-
+                    class="text-2xl font-bold text-white mb-10 fadein-bot title-section flex items-center justify-center flex-col">
                     <h4>Personal project</h4>
-
                 </div>
             </header>
+
             <section>
-                <div>
-                    <div class="md:grid-cols-3 md:gap-3 xl:grid-cols-3 xl:gap-3 2xl:gap-5 fade-zoom-in">
-                        <iframe style="width: 100%; height: 1000px;" src="/files/Jingwen_Dou_Resume.pdf"></iframe>
-                    </div>
+                <div class="md:grid-cols-3 md:gap-3 xl:grid-cols-3 xl:gap-3 2xl:gap-5 fade-zoom-in">
+                    <!-- Use imported PDF URL -->
+                    <iframe
+                        :src="resumeUrl"
+                        style="width: 100%; height: 1000px;"
+                    ></iframe>
                 </div>
             </section>
         </article>
@@ -28,5 +34,4 @@
 </template>
 
 <style>
-
 </style>
