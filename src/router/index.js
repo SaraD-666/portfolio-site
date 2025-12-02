5b1783bafc9b590f461a7f140de40b41b461a2e3
3047322836 <3047322836@qq.com>
2025-12-01 19:50:03 -0500

    it's just updating the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/IndexView.vue'
 
 const routes = [
@@ -48,7 +48,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
